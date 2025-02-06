b49e75218df33246eb3a034896c6645d93c1384f
Emmanuel <sarpbe808@gmail.com>
2025-02-06 18:04:40 +0000

    setting componet types Dynamically

--- React_Essentials_1/0x001_Alternative_prop_syntax/src/component/Tabs.jsx
+++ React_Essentials_1/0x001_Alternative_prop_syntax/src/component/Tabs.jsx
@@ -1,4 +1,6 @@
-export default function Tabs({ children, buttons }) {
+export default function Tabs({ children, buttons, ButtonsContainer }) {
+    // can use "buttonContainers" with a samll letter initiable
+    // when that is done, react will think of it a bbbbbbbbbbbbbbbbbb                                                                                                                                                                                                                   bbbb                                                                                                                                                                                                                                                                                                                                                       bbbbb
   return (
     <>
       <menu>{buttons}</menu>
