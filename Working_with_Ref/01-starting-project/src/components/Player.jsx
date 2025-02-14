8177647a161b04d52732b342ec6b0d79693f59f2
Emmanuel <sarpbe808@gmail.com>
2025-02-14 20:59:45 +0000

    Using Ref to access Html

--- Working_with_Ref/01-starting-project/src/components/Player.jsx
+++ Working_with_Ref/01-starting-project/src/components/Player.jsx
@@ -1,23 +1,28 @@
+import { useRef } from "react";
 import { useState } from "react";
 
 export default function Player() {
+  //  Accessing HTML elements via Ref()
+  const playerName = useRef();
 
-  const [enteredPlayerName, setEnteredPlayerName] =useState(null);
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
   const [submitted, setSubmitted] = useState(false);
 
-  function handleChange(event){
-    setSubmitted(false)
-    setEnteredPlayerName(event.target.value)
-  }
-  function handleClick(){
-    setSubmitted(true)
+  // function handleChange(event) {
+  //   setSubmitted(false);
+  //   setEnteredPlayerName(event.target.value);
+  // }
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value);
+    // Clearing the input box manually
+    playerName.current.value = "";
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName:"unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
