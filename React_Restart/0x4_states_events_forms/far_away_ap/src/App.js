a5678805806615a1e93d27f03c6f55e72f61b641
Emmanuel <sarpbe808@gmail.com>
2025-03-08 11:01:28 -1200

    States

--- React_Restart/0x4_states_events_forms/far_away_ap/src/App.js
+++ React_Restart/0x4_states_events_forms/far_away_ap/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,21 +22,39 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuauntity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+
+    const newList = { description, quantity, package: false, id: Date.now() };
+    console.log(newList);
+
+    setDescription("");
+    setQuauntity(1);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip</h3>
       {/* An array to get 20 numbers together with map */}
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuauntity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (cur, index) => index + 1).map((num) => (
-          <option value={num} key={num}>
+          <option key={num} value={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="text..." />
+      <input
+        type="text"
+        placeholder="text..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>ADD</button>
     </form>
   );
