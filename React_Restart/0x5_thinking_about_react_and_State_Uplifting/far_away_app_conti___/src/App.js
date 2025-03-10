15e77b0ba1ef78de6102242eb49ddc77cd917148
Emmanuel <sarpbe808@gmail.com>
2025-03-09 14:56:31 -1200

    Sorting Items by the use of derive state

--- React_Restart/0x5_thinking_about_react_and_State_Uplifting/far_away_app_conti___/src/App.js
+++ React_Restart/0x5_thinking_about_react_and_State_Uplifting/far_away_app_conti___/src/App.js
@@ -91,10 +91,29 @@ function Form({ onAddItems }) {
 
 function PackingList({ item, onDeleteItem, onToggleItem }) {
   // console.log(item);
+
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = item;
+
+  if (sortBy === "description")
+    sortedItems = item
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = item
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  // else{
+  //   setSortBy(item.slice)
+  // }
   return (
     <div className="list">
       <ul>
-        {item.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -103,6 +122,15 @@ function PackingList({ item, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className="action">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed</option>
+        </select>
+        <button>Clear</button>
+      </div>
     </div>
   );
 }
